c7489ed3cb001098fe88c888cdd343116f3d0c68
mikefaraponov <mikefaraponov@gmail.com>
2018-11-02 01:51:59 +0200

    Refactored UI

--- ui.go
+++ ui.go
@@ -7,61 +7,107 @@ import (
 	"time"
 )
 
-func NewUI(user User, client chatum.Chatum_CommunicateClient) (tui.UI, error) {
-	history := tui.NewVBox()
-	historyScroll := tui.NewScrollArea(history)
+type terminalUI struct {
+	User
+	chatum.Chatum_CommunicateClient
+	tui.UI
+	history    *tui.Box
+	historyBox *tui.Box
+	inputBox   *tui.Box
+	chat       *tui.Box
+	input      *tui.Entry
+}
+
+func (ui *terminalUI) addHistory() *terminalUI {
+	ui.history = tui.NewVBox()
+	historyScroll := tui.NewScrollArea(ui.history)
 	historyScroll.SetAutoscrollToBottom(true)
-	historyBox := tui.NewVBox(historyScroll)
-	historyBox.SetBorder(true)
-	input := tui.NewEntry()
-	input.SetFocused(true)
-	input.SetSizePolicy(tui.Expanding, tui.Maximum)
-	inputBox := tui.NewHBox(input)
-	inputBox.SetBorder(true)
-	inputBox.SetSizePolicy(tui.Expanding, tui.Maximum)
-	chat := tui.NewVBox(historyBox, inputBox)
-	chat.SetSizePolicy(tui.Expanding, tui.Expanding)
-	input.OnSubmit(func(e *tui.Entry) {
-		if err := client.Send(NewMessage(e.Text())); err != nil {
-			history.Append(tui.NewHBox(
-				tui.NewLabel(time.Now().Format(TimeFormat)),
-				tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf(UserLabelTemplate, string(user)))),
-				tui.NewLabel(MessageNotDeliveredPrompt),
-				tui.NewSpacer(),
-			))
-		}
-		history.Append(tui.NewHBox(
+	ui.historyBox = tui.NewVBox(historyScroll)
+	ui.historyBox.SetBorder(true)
+	return ui
+}
+
+func (ui *terminalUI) addInput() *terminalUI {
+	ui.input = tui.NewEntry()
+	ui.input.SetFocused(true)
+	ui.input.SetSizePolicy(tui.Expanding, tui.Maximum)
+	ui.input.OnSubmit(ui.onSubmit)
+	ui.inputBox = tui.NewHBox(ui.input)
+	ui.inputBox.SetBorder(true)
+	ui.inputBox.SetSizePolicy(tui.Expanding, tui.Maximum)
+	return ui
+}
+
+func (ui *terminalUI) addChat() *terminalUI {
+	ui.chat = tui.NewVBox(ui.historyBox, ui.inputBox)
+	ui.chat.SetSizePolicy(tui.Expanding, tui.Expanding)
+	return ui
+}
+
+func (ui *terminalUI) onSubmit(e *tui.Entry) {
+	if err := ui.Send(NewMessage(e.Text())); err != nil {
+		ui.history.Append(tui.NewHBox(
 			tui.NewLabel(time.Now().Format(TimeFormat)),
-			tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf(UserLabelTemplate, string(user)))),
-			tui.NewLabel(e.Text()),
+			tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf(UserLabelTemplate, string(ui.User)))),
+			tui.NewLabel(MessageNotDeliveredPrompt),
 			tui.NewSpacer(),
 		))
-		input.SetText(CleanInput)
-	})
-	ui, err := tui.New(tui.NewHBox(chat))
-	go func() {
-		for {
-			if msg, err := client.Recv(); err != nil {
-				history.Append(tui.NewHBox(
-					tui.NewLabel(time.Now().Format(TimeFormat)),
-					tui.NewPadder(1, 0, tui.NewLabel(ErrorLabel)),
-					tui.NewLabel(PleaseRestartPrompt),
-					tui.NewSpacer(),
-				))
-				ui.Update(DoNothing)
-				return
-			} else if msg.GetType() == chatum.EventType_DEFAULT {
-				history.Append(tui.NewHBox(
-					tui.NewLabel(time.Now().Format(TimeFormat)),
-					tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf(UserLabelTemplate, msg.GetUsername()))),
-					tui.NewLabel(msg.GetMessage()),
-					tui.NewSpacer(),
-				))
-				ui.Update(DoNothing)
-			} else if msg.GetType() == chatum.EventType_PING {
-				client.Send(NewPongMessage())
-			}
+	}
+	ui.history.Append(tui.NewHBox(
+		tui.NewLabel(time.Now().Format(TimeFormat)),
+		tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf(UserLabelTemplate, string(ui.User)))),
+		tui.NewLabel(e.Text()),
+		tui.NewSpacer(),
+	))
+	ui.input.SetText(CleanInput)
+}
+
+func (ui *terminalUI) buildUI() (terminal tui.UI, err error) {
+	terminal, err = tui.New(tui.NewHBox(ui.chat))
+	if err != nil {
+		return
+	}
+	ui.UI = terminal
+	go ui.onNewMessage()
+	return
+}
+
+func (ui *terminalUI) onNewMessage() {
+	for {
+		if msg, err := ui.Recv(); err != nil {
+			ui.history.Append(tui.NewHBox(
+				tui.NewLabel(time.Now().Format(TimeFormat)),
+				tui.NewPadder(1, 0, tui.NewLabel(ErrorLabel)),
+				tui.NewLabel(PleaseRestartPrompt),
+				tui.NewSpacer(),
+			))
+			ui.Update(DoNothing)
+			return
+		} else if msg.GetType() == chatum.EventType_DEFAULT {
+			ui.history.Append(tui.NewHBox(
+				tui.NewLabel(time.Now().Format(TimeFormat)),
+				tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf(UserLabelTemplate, msg.GetUsername()))),
+				tui.NewLabel(msg.GetMessage()),
+				tui.NewSpacer(),
+			))
+			ui.Update(DoNothing)
+		} else if msg.GetType() == chatum.EventType_PING {
+			ui.Send(NewPongMessage())
 		}
-	}()
-	return ui, err
+	}
+}
+
+func newTerminalUI(user User, client chatum.Chatum_CommunicateClient) *terminalUI {
+	return &terminalUI{
+		User:                     user,
+		Chatum_CommunicateClient: client,
+	}
+}
+
+func NewUI(user User, client chatum.Chatum_CommunicateClient) (tui.UI, error) {
+	return newTerminalUI(user, client).
+		addHistory().
+		addInput().
+		addChat().
+		buildUI()
 }
